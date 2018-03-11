3d855addbb64f2fa71c54d0249aee6d968076b0a
Kevin <huanqing19901001@163.com>
2018-03-11 18:52:06 +0800

    A rails web application.

---
+++ weibo_demo/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user and redirect to the user's shoe page.
+      sign_in user
+      #redirect_to user
+      redirect_back_or user
+    else
+      # Create an error message and rerender the sign in form.
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+end
---
+++ weibo_demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'a95f80d7e0cabad8dcd4bdf7a02860f55f8dbfb94147e6a7079c70079c024122ae509cc7600a05cd35b1aa02a2b4074d5c57af33df483408b46dd1a630f1ea78'
